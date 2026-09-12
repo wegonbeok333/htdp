7f198c4bafa8ec24d1132ac8243f816af67d91df
philipp <haha@you.me>
2026-09-12 23:24:17 +0200

    exercise 1

---
+++ chapter1/exercise1.rkt
@@ -0,0 +1,8 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname exercise1) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;Exercise 1
+(define x 3)
+(define y 4)
+
+(sqrt (+ (sqr x) (sqr y)))
